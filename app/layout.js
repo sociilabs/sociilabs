76eade9da77b26b91a87efee03aa459ea6d3529d
Muhammad Arslan Aslam <hello@arslanaslam.me>
2024-07-09 22:40:30 +0500

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const SpaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={SpaceGrotesk.className}>{children}</body>
+      <GoogleAnalytics gaId={`G-XMGN485TPP`} />
     </html>
   );
 }
