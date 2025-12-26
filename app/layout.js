0125712f9b79df9d547dfc6ca451588a13bea6f4
Muhammad Arslan Aslam <hello@arslanaslam.me>
2025-12-27 00:11:33 +0500

    add intercom

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Intercom from '@intercom/messenger-js-sdk';
 
 const SpaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -10,6 +11,10 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  Intercom({
+    app_id: 'p6d30xsk',
+  });
+  
   return (
     <html lang="en">
       <body className={SpaceGrotesk.className}>{children}</body>
